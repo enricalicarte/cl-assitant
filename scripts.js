4d426ee27019b9add30c50cab0b5baab7de350c7
enricalicarte <enricalicarte@gmail.com>
2024-12-13 12:56:05 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -21,7 +21,7 @@ async function sendMessage() {
     const loadingMessage = addMessage("Escribiendo...", "bot");
 
     try {
-        const response = await fetch("https://tu-n8n-url/webhook", {
+        const response = await fetch("https://multiplicaenric.app.n8n.cloud/webhook-test/527dea54-5355-4717-bbb7-59ecd936269b", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({
@@ -57,7 +57,7 @@ async function sendMessage() {
 // Enviar valoración al Webhook
 async function sendRating(question, answer, rating) {
     try {
-        const response = await fetch("https://tu-n8n-url/webhook", {
+        const response = await fetch("https://multiplicaenric.app.n8n.cloud/webhook-test/527dea54-5355-4717-bbb7-59ecd936269b", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({
